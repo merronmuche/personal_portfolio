b4dba6011218bba43dc703c1d20347d47c125fe6
meron <merronmuche@gmail.com>
2022-11-10 09:28:37 +0300

    add data in the data base

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
