7f40e9c60d98ef4dae0371030ce33d11e1654351
meron <merronmuche@gmail.com>
2022-11-12 13:55:03 +0300

    delete hello world app.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'description','technology', 'image']
+
+admin.site.register(Project, ProjectAdmin)
--- projects/views.py
+++ projects/views.py
@@ -3,15 +3,20 @@ from django.shortcuts import render
 from projects.models import Project
 
 def project_index(request):
+    ''' for listing projects.'''
     projects = Project.objects.all()
     context = {
-        'projects': projects
+      'projects':  projects 
     }
-    return render(request, 'project-index.html', context)
+    return render(request,'project_index.html', context)
 
-def project_detail(request, pk):
-    project = Project.objects.get(pk=pk)
-    context = {
-    'project': project
+def project_detail(request,pk):
+    '''
+    to detail page of project.
+    '''
+    project=Project.objects.get(pk=pk)
+    context ={ 
+        'project':project
     }
-    return render(request, 'project_detail.html', context)
+    return render(request,'project_detail.html',context)
+    
